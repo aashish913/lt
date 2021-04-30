df0a1d58a8e6b99e71df2be3c5930f83e47bbbf0
aashish913 <83440809+aashish913@users.noreply.github.com>
2021-04-30 11:40:50 +0530

    Update solution1_regex.py

--- solution1_regex.py
+++ solution1_regex.py
@@ -30,7 +30,7 @@ def get_args():
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
-
+ 
     args = get_args()
     inputs = args.inputs
     text = args.file.read().rstrip()
